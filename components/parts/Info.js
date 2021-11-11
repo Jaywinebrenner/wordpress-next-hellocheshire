7c9708a1f5149e43dba1209f0ebd2d034c0efb5d
Jay WInebrenner <jaywinebrenner@Jays-MacBook-Pro.local>
2021-11-11 11:29:24 -0800

    add mobile view

--- components/parts/Info.js
+++ components/parts/Info.js
@@ -5,11 +5,11 @@ export default function Info({title, url, alt, body}) {
     return (
       <div className="info">
 
-          <div className="info__left-col">
+          <div className="info__image-col">
              <img src={url} alt={alt}/> 
           </div>
 
-          <div className="info__right-col">
+          <div className="info__text-col">
             <h1>{title}</h1>
             <p>{body}</p>
           </div>
